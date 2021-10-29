7c4cd75349036e99c31c8ca6f3700ea5fedff61b
imen-rahal <imenerahal96@gmail.com>
2021-10-29 20:42:51 +0200

    props

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Profile from "./profile/Profile";
+import image from "./profile/logo.jpg";
+
+function App() {
+  const showAlert = (name) => alert(user.fullNAME);
+  const user = {
+    fullNAME: "Imen RAHAL",
+    bio: "Developpeur Web",
+    profession: `je suis étudiante chercheuse en Méthodes Quantitatives.`,
+  };
+  return (
+    <div className="App">
+      <Profile user={user}
+       showname={showAlert}>
+        <img src={image} alt="myPhoto" />
+      </Profile>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/profile/Profile.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import PropTypes from "prop-types";
+
+  const Profile = (props) => {
+  console.log(props);
+  return (
+    <div className="card" style={myStyle}>
+      <h1 className="card-header" style={{ color: "red" }}>
+        {props.user.fullNAME}
+        <span style={{ marginLeft: 300 }}>{props.children}</span>
+      </h1>
+      <div className="card-body">
+        <h3 className="card-title"  style={{ color: "red" }} >{props.user.bio}</h3>
+        <p className="card-text">{props.user.profession}</p>
+        <a
+          href="#!"
+          className="btn btn-primary" 
+          onClick={(event) => {
+            event.preventDefault();
+            props.showname();
+          }}
+        > c'est moi
+        </a>
+      </div>
+    </div>
+  );
+};
+Profile.propTypes = { user: PropTypes.object.isRequired,showname: PropTypes.func.isRequired };
+Profile.defaultProps = {
+  user: { fullNAME: "admin", bio: "admin", profession: "admin" },
+};
+const myStyle = { width: "50vw", marginLeft: 300 };
+
+export default Profile;
